8e7300778e9fd379b2d4c67e8faf5be894d78656
LSWarss <ls.warss98@gmail.com>
2020-10-25 12:16:13 +0100

    Some random non sec commit

--- ToIssue/ToIssue/settings.py
+++ ToIssue/ToIssue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(i7rne9fmw1kb3a4(*x7k_v16b4psfekg9j#u795$p^u93su_1'
+SECRET_KEY = DJANGO_SECRET_TOKEN
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["b799a4002d3e.ngrok.io",]
 
 
 # Application definition
