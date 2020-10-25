5ed349514a510aa34458a484e232ec780c877fa8
LSWarss <ls.warss98@gmail.com>
2020-10-25 12:34:41 +0100

    Added print to data, for testing

--- ToIssue/webhooks/views.py
+++ ToIssue/webhooks/views.py
@@ -1,4 +1,5 @@
 import json
+import pprint
 from django.shortcuts import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
@@ -9,5 +10,5 @@ from django.views.decorators.csrf import csrf_exempt
 def webhooks_endpoint(request):
     jsondata = request.body
     data = json.loads(jsondata)
-
+    pprint(data)
     return HttpResponse(status=200)
