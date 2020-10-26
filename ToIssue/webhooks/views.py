bec31a9a18e98e90e053afa2c30a03e9c0f8adb3
LSWarss <ls.warss98@gmail.com>
2020-10-26 18:25:37 +0100

    Little correction

--- ToIssue/webhooks/views.py
+++ ToIssue/webhooks/views.py
@@ -12,5 +12,7 @@ def webhooks_endpoint(request):
     data = json.loads(jsondata)
     print(type(data))
     print(data['sender'])
+    for x, y in data['sender']:
+        print(x,y)
     
     return HttpResponse(status=200)
