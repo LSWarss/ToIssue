e6284dff9d8a86d3a09ba547fc5114d42c4b602f
LSWarss <ls.warss98@gmail.com>
2020-10-26 18:13:35 +0100

    Little correction

--- ToIssue/webhooks/views.py
+++ ToIssue/webhooks/views.py
@@ -8,11 +8,9 @@ from django.views.decorators.csrf import csrf_exempt
 @require_POST
 def webhooks_endpoint(request):
     headers = request.headers
-    print(headers)
     jsondata = request.body
     data = json.loads(jsondata)
     for props in data:
-        if props == 'committer':
-            print(props)  
+        print props 
     
     return HttpResponse(status=200)
